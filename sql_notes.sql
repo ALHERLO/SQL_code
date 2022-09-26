eb8aa56bb3adaea3e427c36b2508eec821f81545
Alan Hernandez <101542139+ALHERLO@users.noreply.github.com>
2022-09-25 22:10:51 -0700

    Update sql_notes.sql

--- sql_notes.sql
+++ sql_notes.sql
@@ -364,6 +364,40 @@ WITH t1 AS(SELECT a.id AS id, SUM(o.total_amt_usd)
 
 SELECT * FROM t2;
 /*---------------------------------------------------------------------------*/
+LEFT RIGHT AND LENGHT
+
+1.In the accounts table, there is a column holding the website for each company. The last three 
+digits specify what type of web address they are using. A list of extensions (and pricing) 
+is provided here. Pull these extensions and 
+provide how many of each website type exist in the accounts table.
+
+SELECT RIGHT(website, 3) AS extension, COUNT(*)
+FROM accounts
+GROUP BY 1
+
+2. There is much debate about how much the name (or even the first letter of a company name) 
+matters. Use the accounts table to pull the first letter of each company name to see the 
+distribution of company names that begin with each letter (or number).
+
+SELECT LEFT(UPPER(name), 1) AS initial, COUNT(*)
+FROM ACCOUNTS
+GROUP BY 1
+ORDER BY 2 DESC
+
+3. Use the accounts table and a CASE statement to create two groups: one group 
+of company names that start with a number and a second group of those company 
+names that start with a letter. What proportion of company names start with a 
+letter?
+
+4. Consider vowels as a, e, i, o, and u. What proportion of company names start 
+with a vowel, and what percent start with anything else?
+
+WITH t1 AS(
+    SELECT name, CASE WHEN LEFT(UPPER(name), 1) IN('A', 'E', 'I', 'O', 'U')
+      THEN 1 ELSE 0 AS vowel
+)
+SELECT SUM(vowel) AS total_vowels
+FROM t1
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
