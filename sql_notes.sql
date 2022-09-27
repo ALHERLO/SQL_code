0630e7974f27c03b18d6d78b84f65f872404fc95
Alan Hernandez <101542139+ALHERLO@users.noreply.github.com>
2022-09-26 20:52:59 -0700

    Update sql_notes.sql

--- sql_notes.sql
+++ sql_notes.sql
@@ -364,7 +364,7 @@ WITH t1 AS(SELECT a.id AS id, SUM(o.total_amt_usd)
 
 SELECT * FROM t2;
 /*---------------------------------------------------------------------------*/
-LEFT RIGHT AND LENGHT
+LEFT RIGHT AND LENGTH
 
 1.In the accounts table, there is a column holding the website for each company. The last three 
 digits specify what type of web address they are using. A list of extensions (and pricing) 
@@ -399,6 +399,80 @@ WITH t1 AS(
 SELECT SUM(vowel) AS total_vowels
 FROM t1
 /*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+'POSITION. STRPOS, UPPER & LOWER'
+
+'1. Use the accounts table to create first and last name columns that hold the 
+first and last names for the primary_poc.'
+
+SELECT primary_poc,
+POSITION(' ' IN primary_poc) AS space_pos,
+LEFT(primary_poc, (POSITION(' ' IN primary_poc)-1)) AS first_name,
+RIGHT(primary_poc, (LENGTH(primary_poc)-POSITION(' ' IN primary_poc))) AS last_name
+FROM accounts
+
+'2.Now see if you can do the same thing for every rep name in the sales_reps 
+table. Again provide first and last name columns.'
+
+SELECT name,
+POSITION(' ' IN name) AS space_pos,
+LEFT(name, (POSITION(' ' IN name)-1)) AS first_name,
+RIGHT(name, (LENGTH(name)-POSITION(' ' IN name))) AS last_name
+FROM sales_reps
+
+
+'STRPOS(primary_poc, ' ') 👉🏽SAME AS POSITION BUT DIFFERENT SYNTAX'
+/*---------------------------------------------------------------------------*/
+'CONCAT & || 👉🏽 combines columns together across rows
+
+CONCAT(first_column, ' ', second_column) 
+or
+first_column || ' ' || second_column     -- 👉🏽 piping'
+
+'1.Each company in the accounts table wants to create an email address for each 
+primary_poc. The email address should be the first name of the primary_poc . 
+last name primary_poc @ company name .com.'
+
+SELECT name, 
+LEFT(primary_poc, (STRPOS(primary_poc, ' ') - 1)) AS first_name,
+RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ') ) AS last_name,
+CONCAT(first_name, '.', last_name) AS email
+
+FROM accounts
+
+
+'2.You may have noticed that in the previous solution some of the company names 
+include spaces, which will certainly not work in an email address. See if you 
+can create an email address that will work by removing all of the spaces in the 
+account name, but otherwise your solution should be just as in question 1. Some 
+helpful documentation is here.'
+
+SELECT company_name,
+CONCAT(first_name, '.', last_name, '@', company_name, '.com') AS email
+FROM (SELECT
+      	REPLACE(name, ' ', '') as company_name, 
+      LEFT(primary_poc, (STRPOS(primary_poc, ' ') - 1)) AS first_name,
+	  RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ') ) AS last_name
+FROM accounts)t1
+
+'3.We would also like to create an initial password, which they will change after 
+their first log in. The first password will be the first letter of the primary_pocs 
+first name (lowercase), then the last letter of their first name (lowercase), 
+the first letter of their last name (lowercase), the last letter of their last 
+name (lowercase), the number of letters in their first name, the number of letters 
+in their last name, and then the name of the company they are working with, all 
+capitalized with no spaces.'
+
+SELECT name, company_name,
+CONCAT(first_name, '.', last_name, '@', company_name, '.com') AS email, 
+CONCAT(LOWER(LEFT(first_name, 1)), LOWER(RIGHT(first_name, 1)), 
+LOWER(LEFT(last_name, 1)), LOWER(RIGHT(last_name, 1)), LENGTH(first_name), 
+LENGTH(last_name), UPPER(company_name)) AS password
+FROM (SELECT primary_poc AS name, 
+      	REPLACE(name, ' ', '') as company_name, 
+      LEFT(primary_poc, (STRPOS(primary_poc, ' ') - 1)) AS first_name,
+	  RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ') ) AS last_name
+FROM accounts)t1
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*/
