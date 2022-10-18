e925cf67df2a9cc4078116215076439bfa74d00f
Alan Hernandez <101542139+ALHERLO@users.noreply.github.com>
2022-10-18 09:13:12 -0700

    Update sql_notes.sql

--- sql_notes.sql
+++ sql_notes.sql
@@ -494,4 +494,28 @@ SELECT CAST(new_date AS date) AS casted_date_1,
         new_date::date AS casted_date_2
         FROM t2
 /*---------------------------------------------------------------------------*/
+
+'WINDOW FUNCTIONS 👉🏽 a window function performs a calculation across a set of 
+table rows that are somehow related to the current row'
+
+'1. This time, create a running total of standard_amt_usd (in the orders table) 
+over order time with no date truncation. Your final table should have two 
+columns: one with the amount being added for each new row, and a second with 
+the running total.'
+
+SELECT standard_amt_usd,
+SUM(standard_amt_usd) OVER (ORDER BY occurred_at) AS running_total 👉🏽 order by creates an accumulated sum
+FROM orders
+
+'2. create a running total of standard_amt_usd (in the orders table) over order 
+time, but this time, date truncate occurred_at by year and partition by that 
+same year-truncated occurred_at variable. Your final table should have three 
+columns: One with the amount being added for each row, one for the truncated 
+date, and a final column with the running total within each year.'
+
+SELECT standard_amt_usd,
+DATE_TRUNC('year', occurred_at),
+SUM(standard_amt_usd) OVER (PARTITION BY DATE_TRUNC('year', occurred_at) ORDER BY occurred_at) AS running_total
+FROM orders
 /*---------------------------------------------------------------------------*/
+
